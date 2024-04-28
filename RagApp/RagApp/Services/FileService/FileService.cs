b88ff221f9b8d110ca0e394239f9390aa956c723
Federico D'Armini <fdarmini@progesoftware.it>
2024-04-28 20:10:36 +0200

    done

---
+++ RagApp/RagApp/Services/FileService/FileService.cs
@@ -0,0 +1,30 @@
+﻿namespace RagApp.Services
+{
+    public class FileService : IFileService
+    {
+        private readonly string _basePath;
+
+        public FileService(IWebHostEnvironment env)
+        {
+            _basePath = Path.Combine(env.WebRootPath, "files");
+        }
+
+        public async Task SaveFileAsync(IFormFile file, string path)
+        {
+            var filePath = Path.Combine(_basePath, path);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+        }
+
+        public FileStream GetFile(string path)
+        {
+            var filePath = Path.Combine(_basePath, path);
+            return new FileStream(filePath, FileMode.Open);
+        }
+
+        // Implement other methods similarly
+    }
+
+}
